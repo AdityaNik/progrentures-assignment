97290b90d9a9e41e5e51d83676cb724069d6411e
AdityaNik <aadityanikam2004@gmail.com>
2025-05-12 17:15:15 +0530

    backend connected

--- simple-todo-app/src/App.jsx
+++ simple-todo-app/src/App.jsx
@@ -1,20 +1,47 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react'
+import './App.css';
+import axios from 'axios';
 
 function App() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [todo, setTodo] = useState([]);
 
-  function handleClick() {
+  async function handleClick() {
     console.log("Title: " + title);
     console.log("Description: " + description);
 
+
+    const newTodo = {
+      title, 
+      description
+    }
+
+    const res = await axios.post('http://localhost:3000/todos', newTodo)
+    console.log(res.data);
+
     setTodo([...todo, {title, description}]);
-
     console.log(todo);
-
   }
+
+  async function handleDelete(id) {
+    console.log("ID is: " + id);
+
+    const res = await axios.delete('http://localhost:3000/todos/' + id);
+    console.log(res.data);
+    setTodo(todo.filter(item => item.id !== id));
+  }
+
+
+  useEffect(() => {
+    async function getTodos() {
+      const res = await axios.get('http://localhost:3000/todos');
+      setTodo(res.data);
+  
+      console.log("res is: " + res.data);
+    }
+    getTodos();
+  }, [])
 
 
   return <div>
@@ -36,6 +63,7 @@ function App() {
 
       <div style={{display: 'flex', justifyContent: 'center', marginTop: '20px'}}>
         <button onClick={handleClick}>Add Task</button>
+
       </div>
         <div>
           Todo List Will appear here
@@ -44,7 +72,9 @@ function App() {
         <div style={{display: 'flex', justifyContent: 'space-between', marginTop: '20px'}}>
           <p>{item.title}</p>
           <p>{item.description}</p>
-          <button>Delete</button>
+          <button onClick={() => {
+            handleDelete(item.id)
+          }}>Delete</button>
         </div>
       ))}
     </div>
